d11c6acb1e0eb70df98e349baddb0fcd8828537c
hoyadobi <rkelgl12@naver.com>
2022-05-26 16:18:52 +0900

    hi라고 적혀있음

--- Home/src/exam01/Hello.java
+++ Home/src/exam01/Hello.java
@@ -2,10 +2,6 @@ package exam01;
 
 public class Hello {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello");
-=======
-		System.out.println("Hawefawef");
->>>>>>> branch 'master' of https://github.com/hoyadobi/Exam.git
+		System.out.println("hi");
 	}
 }
